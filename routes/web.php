dcc4343ada205eeb1f2bb8f22f520ba2bab29e45
swengkie88 <angkiprana1987@gmail.com>
2025-12-26 20:06:34 +0700

    feat: Update root route to return JSON response and add /test route for welcome view.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'SIT LHI Boilerplate is running',
+        'version' => '1.0.0',
+    ]);
+});
+
+Route::get('/test', function () {
     return view('welcome');
 });
